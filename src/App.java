56d8fdcc2b94f70eb5e81ae1f090b122c3dfbc60
joaba7981-png <joaba7981@gmail.com>
2025-10-11 15:30:25 -0600

    Update App.java
    Se añadió la clase Persona

--- src/App.java
+++ src/App.java
@@ -1,1 +1,42 @@
+public class Persona{
+
+  private String nombre;
+  private String id;
+  private String tel;
+  private String email;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+  
+}
 
