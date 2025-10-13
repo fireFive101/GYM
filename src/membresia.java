30f6d7b01fa560cba32b23f8aa18d3729b9fba32
joaba7981-png <joaba7981@gmail.com>
2025-10-12 22:20:22 -0600

    Create membresia.java

---
+++ src/membresia.java
@@ -0,0 +1,22 @@
+public class Membresia{
+
+  private String tipo;
+  private double costoMensual;
+
+   public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getCostoMensual() {
+        return costoMensual;
+    }
+
+    public void setCostoMensual(double costoMensual) {
+        this.costoMensual = costoMensual;
+    }
+    
+}
