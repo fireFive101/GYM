5170df2d778a7d50444b77dc8a9b9cb395b3d48b
jdas250606-dotcom <jdas.250606@gmail.com>
2025-10-12 22:06:52 -0600

    Update intructor.java

--- src/intructor.java
+++ src/intructor.java
@@ -1,1 +1,23 @@
+public class Instructor {
+    private String especialidad;
+    private int experienciaAnos;
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public int getExperienciaAnos() {
+        return experienciaAnos;
+    }
+
+    public void setExperienciaAnos(int experienciaAnos) {
+        this.experienciaAnos = experienciaAnos;
+    }
+    
+    
+    
 
