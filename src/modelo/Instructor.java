4b614112068143e2ce53591c481ecf54e318135f
joaba7981-png <joaba7981@gmail.com>
2025-10-20 07:23:51 -0600

    Update Instructor.java
    Corrección de la clase Instructor

--- src/modelo/Instructor.java
+++ src/modelo/Instructor.java
@@ -1,23 +1,26 @@
 package modelo;
 
-public class Instructor {
+// Asumimos que la clase Persona existe y está en el paquete 'modelo'
+public class Instructor extends Persona { 
     private String especialidad;
     private int experienciaAnos;
 
-    public String getEspecialidad() {
-        return especialidad;
-    }
-
-    public void setEspecialidad(String especialidad) {
+    // CONSTRUCTOR (debe llamar a super() para inicializar atributos de Persona)
+    public Instructor(String nombre, String id, String tel, String email, String especialidad, int experienciaAnos) {
+        // Inicializa atributos de Persona
+        super(nombre, id, tel, email); 
         this.especialidad = especialidad;
-    }
-
-    public int getExperienciaAnos() {
-        return experienciaAnos;
-    }
-
-    public void setExperienciaAnos(int experienciaAnos) {
         this.experienciaAnos = experienciaAnos;
     }
-}
 
+    // GETTERS Y SETTERS ya proporcionados en su ejemplo.
+
+    // Método toString() (opcional, pero recomendado)
+    @Override
+    public String toString() {
+        return super.toString() + 
+               "\n  - Rol: Instructor" +
+               "\n  - Especialidad: " + especialidad + 
+               "\n  - Experiencia: " + experienciaAnos + " años";
+    }
+}
